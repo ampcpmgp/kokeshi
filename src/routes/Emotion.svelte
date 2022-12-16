8e68ea245b4192957ea1d0e8c2be50a81760d1f1
ampcpmgp <ampcpmgp@gmail.com>
2022-12-16 21:11:16 +0900

    input/result

--- src/routes/Emotion.svelte
+++ src/routes/Emotion.svelte
@@ -1,1 +1,28 @@
-<div>Emotion Page</div>
+<script>
+  let value = "";
+  let result = "";
+</script>
+
+<main>
+  <h2>Input</h2>
+  <textarea bind:value placeholder="" />
+
+  <button>実行</button>
+
+  <h2>Result</h2>
+  <textarea readonly value={result} placeholder="" />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  textarea {
+    width: 600px;
+    max-width: 80%;
+    height: 140px;
+  }
+</style>
