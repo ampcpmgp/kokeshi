7b03a6557636aecb42be61d3332b22c7d42f9a6f
ampcpmgp <ampcpmgp@gmail.com>
2022-12-17 23:59:30 +0900

    executeDisabled

--- src/routes/Emotion.svelte
+++ src/routes/Emotion.svelte
@@ -1,7 +1,7 @@
 <script>
   import { supabase, tokenP } from "./../supabaseClient";
 
-  const pre = "感情を分類してください:";
+  const pre = "次のチャットの感情を絵文字で分類してください:";
   const suf = "感情:";
 
   let message = ``;
@@ -11,32 +11,39 @@
   let messagePrice = 0;
   let resultPredictionPrice = [0, 0];
   let max_tokens = 64;
+  let executing = false;
   $: prompt = `${pre}\n${message}\n${suf}`;
+  $: executeDisabled = executing || message.length === 0;
 
   async function onExecute() {
+    executing = true;
     const token = tokenP;
     const { data, error } = await supabase.functions.invoke("openai", {
       body: { prompt, max_tokens },
     });
 
-    if (error) throw error;
+    if (error) {
+      executing = false;
+      throw error;
+    }
 
     const choice = data.choices[0];
     result = choice.text;
+    executing = false;
   }
 </script>
 
 <main>
-  <p>あなたが入力したメッセージに含まれる感情を判定することが出来ます。</p>
+  <p>入力したメッセージの感情を絵文字で判定することが出来ます。</p>
 
   <h2>Input</h2>
   <textarea bind:value={message} placeholder="" />
   <span class="lenght">{prompt.length}</span>
 
-  <button on:click={onExecute}>実行</button>
+  <button on:click={onExecute} disabled={executeDisabled}>実行</button>
 
   <h2>Result</h2>
-  <textarea readonly value={result} placeholder="" />
+  <div class="result">{result}</div>
   <span class="lenght">{result.length}</span>
 </main>
 
@@ -47,9 +54,15 @@
     align-items: center;
   }
 
-  textarea {
+  textarea,
+  .result {
     width: 600px;
     max-width: 80%;
     height: 140px;
+  }
+
+  .result {
+    border-radius: 8px;
+    border: solid 1px #ccc;
   }
 </style>
