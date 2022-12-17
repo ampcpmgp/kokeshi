29ab14b4992d0b8b6b698d4e7d564241d7e48dcd
ampcpmgp <ampcpmgp@gmail.com>
2022-12-17 12:06:51 +0900

    !data.session

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+import { push } from "svelte-spa-router";
 import { dataset_dev } from 'svelte/internal'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
@@ -6,5 +7,9 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 export const tokenP = supabase.auth.getSession().then(({ data, error }) => {
+	if (error) throw error
+	if (!data.session) {
+		push("/")
+	}
 	return data.session.access_token
 })
