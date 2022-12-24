50131b1f6a86510b73d00da311f838bb38680e9b
ampcpmgp <ampcpmgp@gmail.com>
2022-12-24 13:22:34 +0900

    dataset_dev

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 import { push } from "svelte-spa-router";
-import { dataset_dev } from 'svelte/internal'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
