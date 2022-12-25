38736ebf8c75f7a498225b50f3f4676938807386
ampcpmgp <ampcpmgp@gmail.com>
2022-12-25 10:41:17 +0900

    特定商取引法に基づく表記

--- src/routes/Law.svelte
+++ src/routes/Law.svelte
@@ -1,1 +1,76 @@
-特定商取引法に基づく表記
+<h2>特定商取引法に基づく表記</h2>
+
+<div class="wrapper">
+  <table>
+    <tbody>
+      <tr>
+        <td>販売者</td>
+        <td>早竹亮</td>
+      </tr>
+      <tr>
+        <td>所在地</td>
+        <td>東京都世田谷区世田谷1-48-17 ロイヤルレジデンス303</td>
+      </tr>
+      <tr>
+        <td>電話番号</td>
+        <td>080-4083-0020</td>
+      </tr>
+      <tr>
+        <td>受付時間</td>
+        <td>14:30 ~ 18:00</td>
+      </tr>
+      <tr>
+        <td>メールアドレス</td>
+        <td>ampcpmgp@gmail.com</td>
+      </tr>
+      <tr>
+        <td>ホームページURL</td>
+        <td>TODO</td>
+      </tr>
+      <tr>
+        <td>販売価格</td>
+        <td>TODO 商品紹介ページをご参照ください。</td>
+      </tr>
+      <tr>
+        <td>商品代金以外の必要料金</td>
+        <td>TODO なし</td>
+      </tr>
+      <tr>
+        <td>引き渡し時期</td>
+        <td>TODO ご注文から3日以内にお届け致します。</td>
+      </tr>
+      <tr>
+        <td>お支払方法</td>
+        <td>PayPay (予定)</td>
+      </tr>
+      <tr>
+        <td>返品・交換・キャンセル</td>
+        <td>TODO 購入から２週間以内なら可能。</td>
+      </tr>
+      <tr>
+        <td>返品期限</td>
+        <td>TODO 商品購入より２週間以内にご連絡下さい。</td>
+      </tr>
+      <tr>
+        <td>返品送料</td>
+        <td>TODO お客様のご負担となります。</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<style>
+  .wrapper {
+    display: grid;
+    place-content: center;
+  }
+
+  td {
+    border: 1px solid #555;
+    padding: 0.6rem;
+  }
+
+  td:first-child {
+    background-color: #555;
+  }
+</style>
