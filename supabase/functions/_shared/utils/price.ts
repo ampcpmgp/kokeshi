3457cebe266962af872afea38fac4f98d9d58ef0
ampcpmgp <ampcpmgp@gmail.com>
2022-12-29 18:47:43 +0900

    update

--- supabase/functions/_shared/utils/price.ts
+++ supabase/functions/_shared/utils/price.ts
@@ -1,4 +1,4 @@
-import { TOKEN_PER_PRICE, TOKEN_PER_WORD } from "../const/price.ts";
+import { PRICE_PER_TOKEN, TOKEN_PER_PRICE, TOKEN_PER_WORD } from "../const/price.ts";
 
 export function convertToTokenFromCredit (credit: number) {
   return Math.floor(credit * TOKEN_PER_PRICE);
@@ -6,4 +6,8 @@ export function convertToTokenFromCredit (credit: number) {
 
 export function convertToTokenFromPrompt (prompt: string) {
   return Math.floor(prompt.length * TOKEN_PER_WORD);
+}
+
+export function getPrice(token: number) {
+  return token * PRICE_PER_TOKEN;
 }
--- supabase/functions/openai/index.ts
+++ supabase/functions/openai/index.ts
@@ -2,86 +2,104 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-import { corsHeaders } from "../_shared/utils/cors.ts"
-import { checkCredit } from "./check-credit.ts"
-import { checkBody } from "./check-body.ts"
-import { getToken } from "./get-token.ts"
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/utils/cors.ts";
+import { checkCredit } from "./check-credit.ts";
+import { checkBody } from "./check-body.ts";
+import { getToken } from "./get-token.ts";
+import { getPrice } from "../_shared/utils/price.ts";
 
 serve(async (req) => {
-  const { method } = req
-	
+  const { method } = req;
+
   // This is needed if you're planning to invoke your function from a browser.
-  if (method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+  if (method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
   }
 
-	const reqData = await req.json()
+  try {
+    const reqData = await req.json();
 
-	try {
-		checkBody(reqData);
-	} catch (error) {
-		return new Response(
-			JSON.stringify({ error: error.message }),
-			{ headers: { ...corsHeaders, "Content-Type": "application/json" }, status: 400 }
-		)
-	}
+    checkBody(reqData);
 
-	const supabaseClient = createClient(
-		Deno.env.get('SUPABASE_URL') ?? '',
-		Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-		{ global: { headers: { Authorization: req.headers.get('Authorization')! } } }
-	)
+    const supabaseClient = createClient(
+      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      {
+        global: {
+          headers: { Authorization: req.headers.get("Authorization")! },
+        },
+      }
+    );
 
     const {
       data: { user },
-			error
-    } = await supabaseClient.auth.getUser()
+      error,
+    } = await supabaseClient.auth.getUser();
 
-		if (error) throw error
-		if (!user) throw new Error("No User")
+    if (error) throw error;
+    if (!user) throw new Error("No User");
 
-		// get balance
-		const { data: balance, error: balanceError } = await supabaseClient.from('balances').select('*').eq('id', user.id).single()
+    // get balance
+    const { data: balance, error: balanceError } = await supabaseClient
+      .from("balances")
+      .select("*")
+      .eq("id", user.id)
+      .single();
 
-		if (balanceError) throw balanceError
+    if (balanceError) throw balanceError;
 
-		try {
-			checkCredit(balance.credit);
-		} catch (error) {
-			return new Response(
-				JSON.stringify({ error: error.message }),
-				{ headers: { ...corsHeaders, "Content-Type": "application/json" }, status: 400 }
-			)
-		}
+    try {
+      checkCredit(balance.credit);
+    } catch (error) {
+      return new Response(JSON.stringify({ error: error.message }), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 400,
+      });
+    }
 
-		const max_tokens = getToken(balance.credit, reqData.prompt)
+    const max_tokens = getToken(balance.credit, reqData.prompt);
 
-	// https://beta.openai.com/docs/api-reference/completions
-  const response = await fetch("https://api.openai.com/v1/completions",
-	 {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Authorization": `Bearer ${Deno.env.get("OPENAI_API_KEY")}`
-		},
-		body: JSON.stringify({
-			model: "text-davinci-003",	
-			prompt: reqData.prompt,
-			// default 16
-			max_tokens,
-		})
-	});
+    // https://beta.openai.com/docs/api-reference/completions
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Deno.env.get("OPENAI_API_KEY")}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: reqData.prompt,
+        // default 16
+        max_tokens,
+      }),
+    });
 
-	const data = await response.json()
+    const data = await response.json();
+    const price = getPrice(data.usage.total_tokens);
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { ...corsHeaders, "Content-Type": "application/json" } },
-  )
-})
+    const { data: latestBalance } = await supabaseClient
+      .from("balances")
+      .select("*")
+      .eq("id", user.id)
+      .single();
 
-/**
-curl --request POST 'http://localhost:54321/functions/v1/openai'   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs'   --header 'Content-Type: application/json'   --data '{ "message":"サンプルメッセージ送信。" }'
-*/
+    await supabaseClient
+      .from("balances")
+      .update({
+        credit: latestBalance.credit - price,
+      })
+      .eq("id", user.id)
+      .single();
+
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
