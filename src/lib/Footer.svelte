73154badea12878153f50460bd60d16ed9f3eac5
ampcpmgp <ampcpmgp@gmail.com>
2022-12-30 12:12:14 +0900

    grid-auto-flow: column

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,20 @@
 <script>
   import { link } from "svelte-spa-router";
+
+  const law = "特定商取引法に基づく表記";
+  const howToUse = "ご利用の流れ";
+  const about = "事業者情報";
 </script>
 
 <footer>
-  <a href="/law" use:link>特定商取引法に基づく表記</a>
-  <a href="/how-to-use" use:link>ご利用の流れ</a>
-  <a href="https://am-public.vercel.app/" target="_blank" rel="noreferrer"
-    >事業者情報
+  <a href="/law" use:link title={law}>{law}</a>
+  <a href="/how-to-use" use:link title={howToUse}>{howToUse}</a>
+  <a
+    href="https://am-public.vercel.app/"
+    target="_blank"
+    rel="noreferrer"
+    title={about}
+    >{about}
     <i class="fa-solid fa-arrow-up-right-from-square" />
   </a>
 </footer>
@@ -17,12 +25,25 @@
     bottom: 0;
     left: 50%;
     transform: translateX(-50%);
-    display: inline-flex;
+    display: inline-grid;
+    grid-auto-flow: column;
     place-content: center;
     gap: 8px;
     padding: 8px 16px;
     border-top: solid 1px #555;
     font-size: 0.8rem;
     background-color: #000;
+  }
+
+  a {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  @media (max-width: 600px) {
+    footer {
+      grid-auto-flow: initial;
+    }
   }
 </style>
