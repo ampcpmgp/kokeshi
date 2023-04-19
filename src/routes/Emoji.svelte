8bac22d613302eced5b947403958ef2db9471f29
ampcpmgp <ampcpmgp@gmail.com>
2023-04-19 19:55:43 +0900

    絵文字変換

---
+++ src/routes/Emoji.svelte
@@ -0,0 +1,76 @@
+<script>
+  import Description from "../lib/Description.svelte";
+  import { Emoji } from "../const/analytics-kind";
+  import Price from "../lib/Price.svelte";
+  import { init } from "../states/openai";
+  import { onMount } from "svelte";
+  import Spacer from "../lib/Spacer.svelte";
+
+  const {
+    description,
+    pre,
+    suf,
+    message,
+    executing,
+    fixedPrice,
+    executeDisabled,
+    minPrice,
+    result,
+    kind,
+    analyzeMessage,
+  } = init();
+
+  onMount(() => {
+    $description = Emoji.DESCRIPTION;
+    $pre = Emoji.PRE;
+    $suf = Emoji.SUF;
+    $kind = "emotion";
+  });
+</script>
+
+<main>
+  <Description>
+    {Emoji.DESCRIPTION}
+  </Description>
+
+  <h2>Input</h2>
+  <textarea bind:value={$message} placeholder="" />
+
+  <Spacer height={2} />
+
+  <button on:click={analyzeMessage} disabled={$executeDisabled}>
+    {$executing ? "実行中.." : "実行"}
+  </button>
+
+  <h2>Result</h2>
+  <div class="result">{$result}</div>
+
+  <Price kind="min" price={$minPrice} />
+  <Price kind="fixed" price={$fixedPrice} />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    place-content: center;
+  }
+
+  textarea,
+  .result {
+    background-color: black;
+    color: white;
+    width: 100%;
+    max-width: 600px;
+    height: 140px;
+  }
+
+  .result {
+    border-radius: 8px;
+    border: solid 1px #ccc;
+    font-size: 60px;
+    line-height: 90px;
+    flex: 1;
+  }
+</style>
