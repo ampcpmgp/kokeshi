601a176dcb06c74d032d02eac307e4e9f72c887b
ampcpmgp <ampcpmgp@gmail.com>
2023-05-12 19:58:54 +0900

    crypto

--- supabase/functions/pay/index.ts
+++ supabase/functions/pay/index.ts
@@ -3,12 +3,18 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createHash } from "https://deno.land/std@0.160.0/hash/mod.ts";
+import { crypto } from "https://deno.land/std@0.186.0/crypto/mod.ts";
 import { corsHeaders } from "../_shared/utils/cors.ts";
 import { toBase64HmacString } from "./toBase64HmacString.ts";
 import { API_URL } from "./const.ts";
 
 const DELIMITER = "\n";
+
+function bin2hex(binary: Uint8Array) {
+  return Array.from(binary)
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}
 
 serve(async (req) => {
   const { method } = req;
@@ -20,26 +26,29 @@ serve(async (req) => {
 
   const { name } = await req.json();
 
-  const hasher = createHash("md5");
-
   const body = JSON.stringify({
-    amount: 100,
+    merchantPaymentId: Deno.env.get("PAYPAY_MERCHANT_ID"),
+    amount: {
+      amount: 100,
+      currency: "JPY",
+    },
     orderDescription: "",
     codeType: "ORDER_QR",
   });
 
   const contentType = "application/json;charset=UTF-8;";
-
-  hasher.update(body);
-  hasher.update(contentType);
-
-  const hash = hasher.toString();
-
+  const encoder = new TextEncoder();
+  const hashBuff = await crypto.subtle.digest(
+    "MD5",
+    encoder.encode(body + contentType)
+  );
+  const hashBin = new Uint8Array(hashBuff);
+  const hash = bin2hex(hashBin);
   const requestUrl = `${API_URL}/codes`;
   const httpMethod = "POST";
-  const nonceArr = crypto.getRandomValues(new Uint8Array(16));
+  const nonceArr = crypto.getRandomValues(new Uint8Array(8));
   const nonce = btoa(String.fromCharCode(...nonceArr));
-  const epoch = Date.now();
+  const epoch = Math.floor(Date.now() / 1000);
   const apiKey = Deno.env.get("PAYPAY_API_KEY") ?? "";
   const secretKey = Deno.env.get("PAYPAY_SECRET_KEY") ?? "";
 
@@ -55,9 +64,11 @@ serve(async (req) => {
    */
   const authHeader = `hmac OPA-Auth:${apiKey}:${hmac}:${nonce}:${epoch}:${hash}`;
 
+  console.log("🚀🐡 ~ file: index.ts:61 ~ serve ~ authHeader:", authHeader);
   const response = await fetch(requestUrl, {
     method: "POST",
     headers: {
+      AUTHORIZATION: authHeader,
       "Content-Type": "application/json;charset=UTF-8;",
       "X-ASSUME-MERCHANT": `Bearer ${Deno.env.get("PAYPAY_MERCHANT_ID")}`,
     },
